7e4e1cd6fbe5fa9f3c5bc28d6d7d58441d9cbc0c
RishuK <krishi8271@gmail.com>
2024-03-19 07:04:54 +0530

    added backgroundChanger

---
+++ 04_bgColorChanger/src/components/Buttons.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+function Buttons (){
+    return(
+        <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+  <button
+    type="button"
+    className="rounded-md bg-blue-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+    
+  >
+    Button text
+  </button>
+  <button
+    type="button"
+    className="rounded-md bg-yellow-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-yellow-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
+  >
+    Button text
+  </button>
+  <button
+    type="button"
+    className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+  >
+    Button text
+  </button>
+  <button
+    type="button"
+    className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+  >
+    Button text
+  </button>
+  <button
+    type="button"
+    className="rounded-md bg-pink-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+  >
+    Button text
+  </button>
+  <button
+    type="button"
+    className="rounded-md bg-blue-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+  >
+    Button text
+  </button>  <button
+    type="button"
+    className="rounded-md bg-orange-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+  >
+    Button text
+  </button>  <button
+    type="button"
+    className="rounded-md bg-purple-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+  >
+    Button text
+  </button>  <button
+    type="button"
+    className="rounded-md bg-purple-300 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+  >
+    Button text
+  </button>
+</div>
+    )
+}
+
+export default Buttons
